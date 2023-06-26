2f64c3e454c1b18e92150015bb28b9091e0ed24c
klinap <kadeklinda83@gmail.com>
2023-06-26 10:12:01 +0800

    ganti ukut

--- projek/dashboard.php
+++ projek/dashboard.php
@@ -56,13 +56,13 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/hi1.png" class="d-block w-100"  alt="...">
+      <img src="img/hi1.png" class="d-block w-50 mx-auto"  alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/vir1.png" class="d-block w-100" alt="...">
+      <img src="img/vir1.png" class="d-block w-50 mx-auto" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/yura1.png" class="d-block w-100" alt="...">
+      <img src="img/yura1.png" class="d-block w-50 mx-auto" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
