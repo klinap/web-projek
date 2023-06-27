15f9b3b8b4ab8338d5921bad62693d900d572ae8
sasmitaputrii <sasmitaputri706@gmail.com>
2023-06-27 08:46:20 +0800

    baru

--- projek/pemesanan.php
+++ projek/pemesanan.php
@@ -1,76 +1,67 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <!--pertama tulis tanda seru lalu enter, maka akan keluar tag html-->
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pesan Tiket Univlox 2023</title>
-</head>
-<body>
-</div>
-</body>
-</html>
 
-<?php //membuat koneksinya
+<?php //pertama kita membuat untuk melakukan koneksinya
 $host = "localhost";
 $user = "root";
-$pass = ""; 
-$db   = "dbtiketkonser";
+$pass = ""; //karena kita menggunakan xampp biasanya pass kosong
+$db   = "dbpesan";
 
-$koneksi=mysqli_connect('localhost','root','','dbtiketkonser');
-    if(!$koneksi)
-    {
-        die("Koneksi Gagal").mysqli_connect_error();
-    }
+$koneksi = mysqli_connect($host, $user, $pass, $db); //untuk koneksi kita menggunakan fungsi dari mysql. lalu kita masukan parameter untuk melakukan koneksi ke database
+if(!$koneksi){ //cek koneksi
+    die("Gagal Koneksi");
+}
 $nama           ="";
 $tanggal_lahir  ="";
 $jenis_tiket    ="";
 $harga_tiket    ="";
-$jam_perform    ="";
 $jumlah_tiket   ="";
-$hari_perform   ="";
 $total_harga    ="";
+$jam_perform    ="";
+$hari_perform   ="";
 $sukses         ="";
 $error          ="";
 
 if(isset($_GET['op'])){
-  $op = $_GET['op'];
+    $op = $_GET['op'];
 }else{
-  $op = "";
+    $op = "";
 }
-
 if($op == 'delete'){
-  if(isset($_GET['id'])){
-      $id = $_GET['id'];
-      $sql1 = "DELETE FROM tbpemesanan WHERE id = '$id'";
-      $q1 = mysqli_query($koneksi, $sql1);
-      if($q1){
-          $sukses = "Berhasil menghapus data";
-      }else{
-          $error = "Gagal menghapus data";
-      }
-  }else{
-      $error = "ID tidak ditemukan";
-  }
-}
+    $id = $_GET['id'];
+    $sql1 = "delete from tb_pemesanan where id = '$id'";
+    $q1 = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil menghapus data";
+    }else{
+        $error = "Gagal menghapus data";
+    }
 
+}
 
 if($op == 'edit'){
     $id             = $_GET['id'];
-    $sql1           = "select *from tbpemesanan where id = '$id'";
+    $sql1           = "select *from tb_pemesanan where id = '$id'";
     $q1             = mysqli_query($koneksi,$sql1);
     $r1             = mysqli_fetch_array($q1);
     $nama           = $r1['nama'];
     $tanggal_lahir  = $r1['tanggal_lahir'];
     $jenis_tiket    = $r1['jenis_tiket'];
     $harga_tiket    = $r1['harga_tiket'];
-    $jam_perform    = $r1['jam_perform'];
     $jumlah_tiket   = $r1['jumlah_tiket'];
-    $hari_perform   = $r1['hari_perform'];
     $total_harga    = $r1['total_harga'];
+    $jam_perform    = $r1['jam_perform'];
+    $hari_perform    = $r1['hari_perform'];
+
     if($nama == ''){
         $error = "Data tidak ditemukan";
     }
+
+
 }
 
 if(isset($_POST['simpan'])){ //untuk create
@@ -78,39 +69,34 @@ if(isset($_POST['simpan'])){ //untuk create
     $tanggal_lahir  = $_POST['tanggal_lahir'];
     $jenis_tiket    = $_POST['jenis_tiket'];
     $harga_tiket    = $_POST['harga_tiket'];
-    $jam_perform_   = $_POST['jam_perform'];
     $jumlah_tiket   = $_POST['jumlah_tiket'];
-    $hari_perform   = $_POST['hari_perform'];
     $total_harga    = $_POST['total_harga'];
- 
+    $jam_perform    = $_POST['jam_perform'];
+    $jam_perform    = $_POST['jam_perform'];
+    $hari_perform   = $_POST['hari_perform'];
 
-    if($nama && $tanggal_lahir && $jenis_tiket && $harga_tiket && $jam_perform && $jumlah_tiket && $hari_perform && $total_harga) {
-      if($op == 'edit'){ //untuk update
-          if(isset($_GET['id'])){
-              $id = $_GET['id'];
-              $sql1 = "UPDATE tbpemesanan SET nama = '$nama', tanggal_lahir = '$tanggal_lahir', jenis_tiket = '$jenis_tiket', harga_tiket = '$harga_tiket', jumlah_tiket = '$jumlah_tiket', total_harga = '$total_harga', jam_perform = '$jam_perform', hari_perform = '$hari_perform' WHERE id = '$id'";
-              $q1 = mysqli_query($koneksi, $sql1);
-              if($q1) {
-                  $sukses = "Data berhasil diupdate";
-              } else {
-                  $error = "Data gagal diupdate";
-              }
-          } else {
-              $error = "ID tidak ditemukan";
-          }
-      } else { //untuk insert
-          $sql1 = "INSERT INTO tbpemesanan(nama, tanggal_lahir, jenis_tiket, harga_tiket, jam_perform, jumlah_tiket, hari_perform, total_harga) VALUES ('$nama', '$tanggal_lahir', '$jenis_tiket', '$harga_tiket', '$jam_perform', '$jumlah_tiket', '$hari_perform', '$total_harga')";
-          $q1 = mysqli_query($koneksi, $sql1);
-          if($q1) {
-              $sukses = "Berhasil memasukkan data baru";
-          } else {
-              $error = "Gagal memasukkan data";
-          }
-      }
-  } else {
-      $error = "Silahkan input semua data";
-  }
-  
+    if($nama && $tanggal_lahir && $jenis_tiket && $harga_tiket && $jumlah_tiket && $total_harga && $jam_perform && $hari_perform){
+        if($op == 'edit'){ //untuk update
+            $sql1 = "update tb_pemesanan set nama = '$nama',tanggal_lahir='$tanggal_lahir',jenis_tiket='$jenis_tiket',harga_tiket='$harga_tiket',jumlah_tiket='$jumlah_tiket',total_harga='$total_harga',jam_perform='$jam_perform',hari_perform='$hari_perform'";
+            $q1 = mysqli_query($koneksi,$sql1);
+            if($q1){
+                $sukses = "Data berhasil diupdate";
+            }else{
+                $error = "Data gagal diupdate";
+            }
+        }else{ //untuk insert
+        $sql1 = "insert into tb_pemesanan (nama,tanggal_lahir,jenis_tiket,harga_tiket,jumlah_tiket,total_harga,jam_perform,hari_perform) values('$nama','$tanggal_lahir','$jenis_tiket','$harga_tiket','$jumlah_tiket','$total_harga','$jam_perform','$hari_perform')";
+        $q1 = mysqli_query($koneksi,$sql1);
+        if($q1){
+            $sukses = "Berhasil memasukkan data baru";
+        }else{
+           
+         $error = "Gagal memasukkan data";
+        }
+        }
+    }else{
+        $error="Silahkan input semua data";
+    }
 }
 
 ?>
@@ -131,7 +117,8 @@ if(isset($_POST['simpan'])){ //untuk create
 
 <body>
     <br><br><br><br>
-    <div class="mx-auto">
+    <div class="mx-auto"> <!--kita berikan class yaitu max auto dia akan membuat sebuah div-->
+    <!--untuk memasukan data-->
     <div class="card">
   <div class="card-header">
     Lakukan Pemesanan Tiket Konser Disini
@@ -144,7 +131,7 @@ if(isset($_POST['simpan'])){ //untuk create
   <?php echo $error ?>
 </div>
             <?php
-            header("refresh:5;url=index.php"); //5 detik
+            header("refresh:5;url=pemesanan.php"); //5 detik
           }
         ?>
       <?php
@@ -154,7 +141,7 @@ if(isset($_POST['simpan'])){ //untuk create
   <?php echo $sukses ?>
 </div>
 <?php
-            header("refresh:5;url=index.php"); //5 detik
+            header("refresh:5;url=pemesanan.php"); //5 detik
           }
         ?>
     <form action="" method="POST">
@@ -167,7 +154,7 @@ if(isset($_POST['simpan'])){ //untuk create
   <div class="mb-3 row">
     <label for="tanggal_lahir" class="col-sm-2 col-form-label">Tanggal Lahir</label>
     <div class="col-sm-10">
-      <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir"value="<?php echo $tanggal_lahir ?>">
+      <input type="text" class="form-control" id="tanggal_lahir" name="tanggal_lahir"value="<?php echo $tanggal_lahir ?>">
     </div>
   </div>
   <div class="mb-3 row">
@@ -175,28 +162,17 @@ if(isset($_POST['simpan'])){ //untuk create
     <div class="col-sm-10">
       <select class="form-control" name="jenis_tiket" id="jenis_tiket">
           <option value="">- Pilih Jenis Tiket -</option>
-          <option value="Diamond_Tribune" <?php if($jenis_tiket == "Diamond_Tribune") echo "selected" ?>>Royal Tribune</option>
-          <option value="Platinum_Tribune" <?php if($jenis_tiket == "Platinum_Tribune") echo "selected" ?>>Platinum Tribune</option>
-          <option value="Gold_Tribune" <?php if($jenis_tiket == "Gold_Tribune") echo "selected" ?>>Gold Tribune</option>
-          <option value="Silver_Tribune" <?php if($jenis_tiket == "Silver_Tribune") echo "selected" ?>>Silver Tribune</option>
+          <option value="diamond_tribune" <?php if($jenis_tiket == "diamond_tribune") echo "selected" ?>>Diamond Tribune</option>
+          <option value="platinum_tribune" <?php if($jenis_tiket == "platinum_tribune") echo "selected" ?>>Platinum Tribune</option>
+          <option value="gold_tribune" <?php if($jenis_tiket == "gold_tribune") echo "selected" ?>>Gold Tribune</option>
+          <option value="silver_tribune" <?php if($jenis_tiket == "silver_tribune") echo "selected" ?>>Silver Tribune</option>
       </select>
-        </div>
+    </div>
   </div>
   <div class="mb-3 row">
     <label for="harga_tiket" class="col-sm-2 col-form-label">Harga Tiket</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="harga_tiket" name="harga_tiket"value="<?php echo $harga_tiket ?>">
-    </div>
-    </div>
-    <div class="mb-3 row">
-    <label for="jam_perform" class="col-sm-2 col-form-label">Jam Perform</label>
-    <div class="col-sm-10">
-      <select class="form-control" name="jam_perform" id="jam_perform">
-          <option value="">- Pilih Jam Perform -</option>
-          <option value="jam_17" <?php if($jenis_tiket == "jam_17") echo "selected" ?>>Jam 17.00</option>
-          <option value="jam_20" <?php if($jenis_tiket == "jam_20") echo "selected" ?>>Jam 20.00</option>
-          <option value="jam_22" <?php if($jenis_tiket == "jam_22") echo "selected" ?>>Jam 22.00</option>
-      </select>
     </div>
   </div>
   <div class="mb-3 row">
@@ -204,29 +180,39 @@ if(isset($_POST['simpan'])){ //untuk create
     <div class="col-sm-10">
       <input type="text" class="form-control" id="jumlah_tiket" name="jumlah_tiket"value="<?php echo $jumlah_tiket ?>">
     </div>
-    </div>
-  <div class="mb-3 row">
-    <label for="hari_perform" class="col-sm-2 col-form-label">Hari ke-</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="hari_perform" name="hari_perform"value="<?php echo $hari_perform ?>">
-    </div>
   </div>
   <div class="mb-3 row">
     <label for="total_harga" class="col-sm-2 col-form-label">Total Harga</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="total_harga" name="total_harga"value="<?php echo $total_harga ?>">
     </div>
+    </div>
+  <div class="mb-3 row">
+    <label for="jam_perform" class="col-sm-2 col-form-label">Jam Perform</label>
+    <div class="col-sm-10">
+      <select class="form-control" name="jam_perform" id="jam_perform">
+          <option value="">- Pilih Jenis Tiket -</option>
+          <option value="jam_17" <?php if($jenis_tiket == "jam_17") echo "selected" ?>>Jam 17.00</option>
+          <option value="jam_20" <?php if($jenis_tiket == "jam_20") echo "selected" ?>>Jam 20.00</option>
+          <option value="jam_22" <?php if($jenis_tiket == "jam_22") echo "selected" ?>>Jam 22.00</option>
+      </select>
+    </div>
+
+    </div>
+    <div class="mb-3 row">
+    <label for="hari_perform" class="col-sm-2 col-form-label">Hari Perform</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="hari_perform" name="hari_perform"value="<?php echo $hari_perform ?>">
+    </div>
   </div>
   <div class="col-12">
       <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary"/>
   </div>
-  <br/>
-  <a href="pemesanan.php?op=edit&id=<?php echo $id?>"><button type="button" class="btn btn-secondary">Buy Ticket</button></a>
     </form>
   </div>
 </div>
 
-<!--untuk menampilkan data-->
+<!--untuk mengeluarkan data-->
 <div class="card">
   <div class="card-header text-white bg-black">
     Lihat Data Anda Disini
@@ -239,17 +225,17 @@ if(isset($_POST['simpan'])){ //untuk create
                 <th scope="col">Nama</th>
                 <th scope="col">Tanggal Lahir</th>
                 <th scope="col">jenis Tiket</th>
-                <th scope="col">Jam Perform</th>
                 <th scope="col">Harga Tiket</th>
                 <th scope="col">Jumlah Tiket</th>
-                <th scope="col">Hari ke-</th>
                 <th scope="col">Total Harga</th>
+                <th scope="col">Jam Perform</th>
+                <th scope="col">Hari Perform</th>
                 <th scope="col">Aksi</th>
             </tr>
     </thead>
             <tbody>
                 <?php
-                $sql2   = "select * from tbpemesanan order by id desc";
+                $sql2   = "select * from tb_pemesanan order by id desc";
                 $q2     = mysqli_query($koneksi,$sql2);
                 $urut   = 1;
                 while($r2 = mysqli_fetch_array($q2)){
@@ -258,10 +244,10 @@ if(isset($_POST['simpan'])){ //untuk create
                     $tanggal_lahir  = $r2['tanggal_lahir'];
                     $jenis_tiket    = $r2['jenis_tiket'];
                     $harga_tiket    = $r2['harga_tiket'];
-                    $jam_perform    = $r2['jam_perform'];
                     $jumlah_tiket   = $r2['jumlah_tiket'];
-                    $hari_perform   = $r2['hari_perform'];
                     $total_harga    = $r2['total_harga'];
+                    $jam_perform    = $r2['jam_perform'];
+                    $hari_perform   = $r2['hari_perform'];
 
                     ?>
                     <tr>
@@ -270,13 +256,13 @@ if(isset($_POST['simpan'])){ //untuk create
                         <td scope="row"><?php echo $tanggal_lahir ?></td>
                         <td scope="row"><?php echo $jenis_tiket ?></td>
                         <td scope="row"><?php echo $harga_tiket ?></td>
-                        <td scope="row"><?php echo $jam_perform ?></td>
                         <td scope="row"><?php echo $jumlah_tiket ?></td>
-                        <td scope="row"><?php echo $hari_perform ?></td>
                         <td scope="row"><?php echo $total_harga ?></td>
+                        <td scope="row"><?php echo $jam_perform ?></td>
+                        <td scope="row"><?php echo $hari_perform ?></td>
                         <td scope="row">
                         <a href="pemesanan.php?op=edit&id=<?php echo $id?>"><button type="button" class="btn btn-secondary">Edit</button></a>
-                        <a href="pemesanan.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau hapus data?')"><button type="button" class="btn btn-danger">Delete</button></a>
+                            <a href="pemesanan.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau hapus data?')"><button type="button" class="btn btn-danger">Delete</button></a>
                         </td>
                     </tr>
                     <?php
